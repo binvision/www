b566ecebbbc943567384cc31e8e6d3b6ccfa7faf
Peter Varga <admin@vargapeter.com>
2022-08-30 15:13:26 +0200

    feat: copy, tooltip, cleanup

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,17 @@
-function copyToClipboard(e){
-		navigator.clipboard.writeText(e.target.innerHtml);
-	
-		/* Alert the copied text */
-		alert("Copied the text: " + e.target.innerHtml);
+function copyToCb(e){
+		e.preventDefault()
+		const el = e.target
+		navigator.clipboard.writeText(el.innerHTML);
+
+		el.classList.add("copy-done");
+		setTimeout(function(){
+			el.classList.remove("copy-done")
+		}.bind(el),2000)		
 }
+
+// add listeners 
+var copyEls = document.querySelectorAll('.js-copy')
+
+copyEls.forEach(function(el){
+  el.addEventListener('click', copyToCb)
+})
