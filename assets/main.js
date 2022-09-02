af5f7b5cea89f86b15fb37eaedbb55b0eb11d45e
Peter Varga <admin@vargapeter.com>
2022-09-03 00:48:36 +0200

    cleanup

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,25 @@
-function copyToCb(e){
-		e.preventDefault()
-		const el = e.target
-		navigator.clipboard.writeText(el.innerHTML);
+function copyToCb(e) {
+  e.preventDefault();
+  const el = e.target;
+  navigator.clipboard.writeText(el.innerHTML);
 
-		el.classList.add("copy-done");
-		setTimeout(function(){
-			el.classList.remove("copy-done")
-		}.bind(el),2000)		
+  el.classList.add("copy-done");
+  setTimeout(
+    function () {
+      el.classList.remove("copy-done");
+    }.bind(el),
+    2000
+  );
 }
 
-// add listeners 
-var copyEls = document.querySelectorAll('.js-copy')
+// add listeners
+var copyEls = document.querySelectorAll(".js-copy");
 
-copyEls.forEach(function(el){
-  el.addEventListener('click', copyToCb)
-})
+copyEls.forEach(function (el) {
+  el.addEventListener("click", copyToCb);
+});
 
-import { Application } from '/assets/runtime.js';
-const canvas = document.getElementById('canvas3d');
+import { Application } from "/assets/runtime.js";
+const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
-app.load('/assets/scene.splinecode');
+app.load("/assets/scene.splinecode");
